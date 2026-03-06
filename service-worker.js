72380d59dc5f05d3c2f577084073c9c18be25e2d
kiz915 <kishorevk40@gmail.com>
2026-03-06 10:19:58 +0530

    Update service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'kishore-portfolio-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/blog.html',
+    '/404.html',
+    '/css/main.css',
+    '/css/darkmode.css',
+    '/css/animations.css',
+    '/js/config.js',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/blog.js',
+    '/manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
